826d4a0addef2a53f02e1b971a3c341e5a8af9dd
Benjamin Chen <yeehenchen@gmail.com>
2018-12-23 12:06:51 +0800

    controller

---
+++ app/helpers/benjamin_helper.rb
@@ -0,0 +1,2 @@
+module BenjaminHelper
+end
---
+++ test/controllers/benjamin_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BenjaminControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
