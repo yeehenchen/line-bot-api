13eea42d2ba689f3e6bf308bb3269563d78bf75f
Benjamin Chen <yeehenchen@gmail.com>
2018-12-26 19:54:04 +0800

    managing backend

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,35 @@
+class LinksController < ApplicationController
+  def index
+    @links = Link.all
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def val_params
+
+  end
+
+  def find_link
+
+  end
+end
