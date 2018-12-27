17dca58c12047a0c8b2329ad1dd18aea6e3b450f
Benjamin Chen <yeehenchen@gmail.com>
2018-12-27 15:01:11 +0800

    remove migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_27_053552) do
+ActiveRecord::Schema.define(version: 2018_12_23_082135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "channels", force: :cascade do |t|
-    t.string "channel_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "links", force: :cascade do |t|
     t.string "word"
