365bffd1fa4a391e56da70bb69d53d0a976aeb9b
Benjamin Chen <yeehenchen@gmail.com>
2019-01-11 16:10:06 +0800

    not yet finished, logix is there

--- app/services/bet_service.rb
+++ app/services/bet_service.rb
@@ -2,17 +2,20 @@ class BetService
   def initialize(user, room, params = [])
     @user = user
     @room = room
-    @balance = params[0].to_i
-    @amount = params[1].to_i
-    @num_guess = params[2].to_i
+    @amount = params[0].to_i
+    @num_guess = params[1].to_i
   end
 
   def bets
-    if Game.where(roomId == @room).select { |g| g.status == false }
-    Bet.new(amount: @amount, num_guess: @num_guess, )
-    if @user.balance >= @balance && b.valid?
-
-      @user.balance -= @balance
+    # check if game exists
+    game = Game.where(roomId == @room).select { |g| g.status == false }
+    # create a bet
+    bet = Bet.new(amount: @amount, num_guess: @num_guess, game_id: game.id, player_id: @user.id)
+    # check if balance is enough -> return err1
+    # then check if bet is valid -> return active record err message
+    if @user.balance >= @amount
+      # if all good then recalc the balance and place bet
+      @user.balance -= @amount
       @user.save!
     end
   end
