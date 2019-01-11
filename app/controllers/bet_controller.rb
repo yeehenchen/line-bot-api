18510b6b16d887cec83e73c75f45dd8f6e1ada14
Benjamin Chen <yeehenchen@gmail.com>
2019-01-11 14:44:44 +0800

    service works, remove route & controller

--- app/controllers/bet_controller.rb
+++
@@ -1,8 +0,0 @@
-class BetController < ApplicationController
-  def bet
-    @player = Player.find(params[:user])
-    @player.balance -= params[:param].to_i
-    @player.save!
-    p @player
-  end
-end
