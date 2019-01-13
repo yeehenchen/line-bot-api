cd8101e653c5f05b9d70781f27f6369e2800d3e9
Benjamin Chen <yeehenchen@gmail.com>
2019-01-13 10:59:51 +0800

    add bets check

--- app/services/end_service.rb
+++ app/services/end_service.rb
@@ -8,6 +8,8 @@ class EndService
     return 'You haven\'t created a game, !start a game first!' if Game.where(roomId: @room).select { |g| g.status == false }.blank?
 
     g = Game.where(roomId: @room).select { |r| r.status == false }.first
+    return 'No one wants to play ?? Crycry' if g.bets.blank?
+
     winbet = g.bets.min_by { |b| (g.winNum - b.num_guess).abs }
     winbet.player.balance += g.bets.sum(:amount)
     winbet.player.save!
