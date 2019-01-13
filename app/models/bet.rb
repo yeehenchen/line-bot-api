bc20de053facb99b4c32b9ea6a0e5be30a4d230e
Benjamin Chen <yeehenchen@gmail.com>
2019-01-13 10:31:02 +0800

    limit one number guessed per player

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -2,7 +2,6 @@ class Bet < ApplicationRecord
   belongs_to :player
   belongs_to :game
 
-  validates :num_guess, presence: true, inclusion: { in: (1..100),
-    message: '%{value} is not a valid size' }
+  validates :num_guess, presence: true, uniqueness: { scope: :player_id, message: 'one number per player' }, inclusion: { in: (1..100), message: '%{value} is not a valid number' }
   validates :amount, presence: true
 end
