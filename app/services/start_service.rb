e90d56eab08f207bb46b29b2c116bd3fde2f12e7
Benjamin Chen <yeehenchen@gmail.com>
2019-01-13 08:20:09 +0800

    debug

--- app/services/start_service.rb
+++ app/services/start_service.rb
@@ -5,9 +5,10 @@ class StartService
   end
 
   def start
-    return 'Already started a game, !end it first to start another one' if Game.where('roomId = ?', @room).each { |r| r.status = false }
+    return 'Already started a game, !end it first to start another one' if Game.where(roomId: @room).each { |r| r.status = false }
 
     Game.create(roomId: @room, winNum: rand(100))
     'Game started!!! See who is closer to the answer.'
   end
 end
+
