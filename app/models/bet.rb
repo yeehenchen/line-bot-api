817f7ca90dce5bc0f964987f73503c166aef5be4
Benjamin Chen <yeehenchen@gmail.com>
2019-01-13 10:33:29 +0800

    add comments

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -2,6 +2,6 @@ class Bet < ApplicationRecord
   belongs_to :player
   belongs_to :game
 
-  validates :num_guess, presence: true, uniqueness: { scope: :player_id, message: 'one number per player' }, inclusion: { in: (1..100), message: '%{value} is not a valid number' }
+  validates :num_guess, presence: true, uniqueness: { scope: :player_id, message: 'error : one number per player' }, inclusion: { in: (1..100), message: 'error : %{value} is not a valid number' }
   validates :amount, presence: true
 end
