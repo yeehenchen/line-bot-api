1f268fd8c82ffbfa5f0f9a556ae58588bba916da
Benjamin Chen <yeehenchen@gmail.com>
2019-01-14 19:49:19 +0800

    modify validation & message

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -2,6 +2,6 @@ class Bet < ApplicationRecord
   belongs_to :player
   belongs_to :game
 
-  validates :num_guess, presence: true, uniqueness: { scope: [:player_id, :game_id], message: 'error : one number per player' }, inclusion: { in: (1..100), message: 'error : %{value} is not a valid number' }
+  validates :num_guess, presence: true, uniqueness: { scope: :game_id, message: 'error : 數字只能下一次' }, inclusion: { in: (1..100), message: 'error : %{value}不在1到100之間' }
   validates :amount, presence: true
 end
