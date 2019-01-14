0cb2b91cdc556b4c428c3ee4b014468218d0298b
Benjamin Chen <yeehenchen@gmail.com>
2019-01-14 20:19:10 +0800

    calling api failed, hasn't registered. try another way instead

--- app/services/rank_service.rb
+++ app/services/rank_service.rb
@@ -9,17 +9,21 @@ class RankService
     return '你們還沒有開始/完成任何一場遊戲ㄛ，用!start開始吧！' if Game.where(roomId: @des).select { |r| r.status == true }.blank?
 
     games = Game.where(roomId: @des).select { |r| r.status == true }
-    players = get_players_from_des(@des)
-  end
-
-  def get_players_from_des(des)
-    players = Hash.new(0)
-    url = "https://api.line.me/v2/bot/#{@type}/#{des}/members/ids"
-    res = open(url, 'Authorization' => "Bearer #{ENV['LINE_TOKEN']}").read
-    res["memberIds"].each do |m|
-      p = Player.where(userId == m)
-      players[p.displayName] = 0
+    rank = Hash.new(0)
+    games.each do |g|
+      rank[g.winner] += 1
     end
-    p players
+    p rank
   end
 end
+# Not available if register
+# def get_players_from_des(des)
+#   players = Hash.new(0)
+#   url = "https://api.line.me/v2/bot/#{@type}/#{des}/members/ids"
+#   res = open(url, 'Authorization' => "Bearer #{ENV['LINE_TOKEN']}").read
+#   res["memberIds"].each do |m|
+#     p = Player.where(userId == m)
+#     players[p.displayName] = 0
+#   end
+#   p players
+# end
