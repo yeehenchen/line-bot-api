9723391ca419120816dc94a8401933d610d286f0
Benjamin Chen <yeehenchen@gmail.com>
2019-01-18 18:28:45 +0800

    winNum to float

--- app/services/end_service.rb
+++ app/services/end_service.rb
@@ -10,7 +10,7 @@ class EndService
     g = Game.where(roomId: @room).select { |r| r.status == false }.first
     return no_bet(g) if g.bets.blank?
 
-    g.winNum = g.bets.average(:num_guess)
+    g.winNum = g.bets.average(:num_guess).to_f
 
     winbet = g.bets.min_by { |b| (g.winNum - b.num_guess).abs }
     winbet.player.balance += (winbet.amount * g.bets.count)
